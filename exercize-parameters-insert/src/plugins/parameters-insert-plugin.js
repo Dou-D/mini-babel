ced1e9896a1a7476bf3cdecb569d14f82b3fac10
dou <15309487104@163.com>
2024-08-26 15:37:57 +0800

    exercize-parameters-insert

---
+++ exercize-parameters-insert/src/plugins/parameters-insert-plugin.js
@@ -0,0 +1,61 @@
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const generate = require("@babel/generator").default;
+const types = require("@babel/types");
+const template = require("@babel/template").default;
+
+const source = `console.log(1)`;
+const sourceCode = `
+    console.log(1);
+
+    function func() {
+        console.info(2);
+    }
+
+    export default class Clazz {
+        say() {
+            console.debug(3);
+        }
+        render() {
+            return <div>{console.error(4)}</div>
+        }
+    }
+`;
+
+const targetCalleeName = ["log", "info", "error", "debug"].map(
+  (item) => `console.${item}`
+);
+
+const ast = parser.parse(sourceCode, {
+  sourceType: "unambiguous",
+  plugins: ["jsx"],
+});
+
+const parametersInsertPlugin = function ({ types, template }) {
+  return {
+    visitor: {
+      CallExpression(path, state) {
+        if (path.node.isNew) {
+          return;
+        }
+        const calleeName = generate(path.node.callee).code;
+        if (targetCalleeName.includes(calleeName)) {
+          const { line, column } = path.node.loc.start;
+          const newNode = template.expression(
+            `console.log("filename: (${line},${column})")`
+          )();
+          newNode.isNew = true;
+
+          if (path.findParent((path) => path.isJSXElement())) {
+            path.replaceWith(types.arrayExpression([newNode, path.node]));
+            path.skip();
+          } else {
+            path.insertBefore(newNode);
+          }
+        }
+      },
+    },
+  };
+};
+
+module.exports = parametersInsertPlugin;
